8434bdb5cb3690ff78f4f3feebc2e2941897a319
Kim Eik <kim@heldig.org>
2013-04-15 21:47:53 +0200

    added static file handler

--- main.go
+++ main.go
@@ -8,9 +8,18 @@ import (
 )
 
 func main() {
+	log.Print("Creating static files handler")
+	staticPfx := "/static/"
+	staticFilesPath := conf.Config.ApplicationHome + staticPfx
+	staticHandler := http.StripPrefix(staticPfx, http.FileServer(http.Dir(staticFilesPath)))
+	http.Handle(staticPfx, staticHandler)
+
+	log.Print("Creating application handler")
+	http.Handle("/", app.WebHandler)
+
 	serverAddr := conf.Config.ServerAddr
 	log.Printf("Starting server, listening on: %s", serverAddr)
-	if err := http.ListenAndServe(serverAddr, app.WebHandler); err != nil {
+	if err := http.ListenAndServe(serverAddr, nil); err != nil {
 		panic(err)
 	}
 }
