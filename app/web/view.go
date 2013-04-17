363cacbacd31d832ae66331614d7ad3982bc4af2
Kim Eik <kim@heldig.org>
2013-04-17 21:58:37 +0200

    added proper view rendering logic and data

--- app/web/view.go
+++ app/web/view.go
@@ -5,6 +5,7 @@ import (
 	"github.com/netbrain/cloudfiler/app/web/fvgo"
 	"html/template"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -30,23 +31,26 @@ func loadTemplates() {
 	log.Println("Parsing templates in: " + ViewDir())
 	filepath.Walk(ViewDir(), func(path string, info os.FileInfo, err error) error {
 		if info != nil && !info.IsDir() {
-			if strings.HasSuffix(info.Name(), ".html") {
+			if !strings.HasPrefix(info.Name(), "_") && strings.HasSuffix(info.Name(), ".html") {
 				log.Println(path)
 
-				tmpl, err := template.New(info.Name()).Funcs(templateFunctions).ParseFiles(path)
+				relPath, _ := filepath.Rel(ViewDir(), path)
+				view := relPath[0:strings.LastIndex(relPath, ".html")]
 
+				baseSrc, err := ioutil.ReadFile(baseFilePath())
 				if err != nil {
 					panic(err)
 				}
 
-				FormValidator.ParseTemplate(path)
-
-				relPath, _ := filepath.Rel(ViewDir(), path)
-				view := relPath[0:strings.LastIndex(relPath, ".html")]
+				tmplSrc, err := ioutil.ReadFile(path)
+				if err != nil {
+					panic(err)
+				}
+				tmpl := parseTemplate(string(baseSrc), string(tmplSrc))
 				Views[view] = tmpl
 
+				FormValidator.ParseTemplate(path)
 				log.Printf("Created view: %s", view)
-
 			}
 		}
 		return nil
@@ -77,8 +81,20 @@ func RenderView(view string, w io.Writer, data interface{}) {
 		panic("view doesn't exist, create it first! " + view)
 	}
 
-	err := tmpl.Execute(w, data)
+	err := tmpl.ExecuteTemplate(w, "base", data)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func baseFilePath() string {
+	return filepath.Join(ViewDir(), "_base.html")
+}
+
+func parseTemplate(src ...string) *template.Template {
+	t := template.New("*").Funcs(templateFunctions)
+	for _, s := range src {
+		t = template.Must(t.Parse(s))
+	}
+	return t
 }
